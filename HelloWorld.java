911f6fa66f32b9e5304471425b718b240a7f94ac
andyk21 <andykim2021@gmail.com>
2020-04-01 03:21:00 +0900

    Initial commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,7 @@ import acm.program.*;
 public class HelloWorld extends ConsoleProgram{
 	public void run() {
 		println("Hello World");
+		String name = readLine("What's your name? ");
+		println("Hello there, " + name + "!");
 	}
 }
